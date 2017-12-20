22bb77e496aa316b66e95b0d0471661cc3a4c87b
Aleksej Zelezniak <alezel@chalmers.se>
2017-12-21 00:33:57 +0100

    adds harmonic mean to octopus_rank.R

--- octopus_rank.R
+++ octopus_rank.R
@@ -50,7 +50,7 @@ non_zeros <- (dataset %>% group_by(name) %>% summarise(zeros = sum(value == 0)) 
 threshold = Z_THR # threshold
 k1 = N # moving average points (8min * K)
 
-dataset.stats <- dataset %>% filter(name %in% non_zeros) %>%
+dataset.stats <- dataset %>% filter(name %in% non_zeros) %>% distinct(name, date, value, data_type) %>%
   group_by(name, data_type) %>%
   mutate(avg = rollapply(value, k1, mean, align ="right", na.rm = TRUE, fill = NA),
          std = rollapply(value, k1, sd, align ="right", na.rm = TRUE, fill = NA), 
@@ -61,14 +61,19 @@ toSave <- dataset.stats %>% ungroup() %>% filter(z > threshold) %>% dplyr::selec
 
 file_path = paste(SAVE_DIR, SAFE_FILE, sep = "/")
 
-toSave %>% 
+toSave %>% rename(coin_id = name) %>% ungroup() %>%
+  mutate(current_time = ymd_hms(Sys.time()),
+         date_diff = as.numeric(current_time - date, units="hours"),
+         diff_inv = 1/date_diff,
+         scaled_diff_inv = scales::rescale(diff_inv, to = c(1,5)),
+         scaled_z = scales::rescale(z, c(1,5)),
+         h_mean = 2*scaled_z*scaled_diff_inv/(scaled_z + scaled_diff_inv)) %>%
   toJSON(pretty = T) %>%
   write_lines(file_path)
 
 dbDisconnect(con)
 
 flog.info("Finished running: %s, total peaks %s for %s cryptos", file_name, nrow(toSave), length(unique(toSave$name)), name = my_logger)
-file.remove(".RData") 
-q("n")
+q("no")
 
 
